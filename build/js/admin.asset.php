45503cae903f17961434c63fc81043097c9b2268
Shehab mahamud Sabbir <mdshehab204@gmail.com>
2024-06-04 00:36:04 +0600

    added

--- build/js/admin.asset.php
+++ build/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => 'a2910e9a153e56c2041b');
+<?php return array('dependencies' => array('jquery', 'react', 'react-dom', 'wp-element'), 'version' => '407e25db7bcc5c5c21a9');
--- build/js/main.asset.php
+++ build/js/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '62b8095c28d0715eaad3');
+<?php return array('dependencies' => array(), 'version' => '31d6cfe0d16ae931b73c');
